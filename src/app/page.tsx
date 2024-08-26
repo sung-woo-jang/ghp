76a2b4cab279fcc9cb52bf00185396cb73c46e7d
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-08-26 15:31:30 +0900

    포트폴리오 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
+// pages/index.js
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import blueRacket from "@/../public/img/blueRacket.png";
+import redRacket from "@/../public/img/redRacket.png";
+import blackRacket from "@/../public/img/blackRacket.png";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <h1 className={styles.title}>장성우의 포트폴리오</h1>
+      <div className={styles.welcome}>
+        안녕하세요. <br />
+        늘 고민하는 개발자 장성우입니다. <br />
+        제 프로젝트들을 소개합니다.
+        <br />
+        <br />
+      </div>
+      <div className={styles.cardContainer}>
+        <div className={styles.card}>
+          <Image
+            className={styles.cardImg}
+            src={redRacket}
+            alt="Project 1"
+            width={300}
+            height={300}
+          />
+          <p className={styles.cardTitle}>프로젝트 1</p>
+          <Link href="/projects/1">
+            <button className={styles.cardButton}>상세보기</button>
+          </Link>
+        </div>
+        <div className={styles.card}>
+          <Image
+            className={styles.cardImg}
+            src={blueRacket}
+            alt="Project 2"
+            width={300}
+            height={300}
+          />
+          <p className={styles.cardTitle}>프로젝트 2</p>
+          <Link href="/projects/2">
+            <button className={styles.cardButton}>상세보기</button>
+          </Link>
+        </div>
+        <div className={styles.card}>
+          <Image
+            className={styles.cardImg}
+            src={blackRacket}
+            alt="Project 3"
+            width={300}
+            height={300}
+          />
+          <p className={styles.cardTitle}>프로젝트 3</p>
+          <Link href="/projects/3">
+            <button className={styles.cardButton}>상세보기</button>
+          </Link>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,53 @@
+import styles from "./../../page.module.css";
+import Link from "next/link";
+import blueRacket from "@/../public/img/blueRacket.png";
+import redRacket from "@/../public/img/redRacket.png";
+import blackRacket from "@/../public/img/blackRacket.png";
+import Image from "next/image";
+interface ProjectPageProps {
+  params: { id: 1 | 2 | 3 };
+}
+
+export default function Project({ params }: ProjectPageProps) {
+  // 이 부분에서 프로젝트 ID에 따라 실제 프로젝트 데이터를 불러오는 로직을 구현할 수 있습니다.
+  const projectData = {
+    1: {
+      title: "프로젝트 1",
+      description: "프로젝트 1에 대한 상세 설명",
+      image: redRacket,
+    },
+    2: {
+      title: "프로젝트 2",
+      description: "프로젝트 2에 대한 상세 설명",
+      image: blueRacket,
+    },
+    3: {
+      title: "프로젝트 3",
+      description: "프로젝트 3에 대한 상세 설명",
+      image: blackRacket,
+    },
+  };
+
+  const project = projectData[params.id];
+
+  if (!project) return <div>프로젝트를 찾을 수 없습니다.</div>;
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{project.title}</h1>
+      <Image
+        className={styles.cardImg}
+        src={projectData[params.id].image}
+        alt="Project 1"
+        width={300}
+        height={300}
+      />
+      <p>{project.description}</p>
+      <Link href="/">
+        <button className={styles.cardButton} style={{ width: "100%" }}>
+          홈으로 돌아가기
+        </button>
+      </Link>
+    </div>
+  );
+}

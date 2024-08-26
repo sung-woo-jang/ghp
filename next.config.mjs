93e49ac08f1dc4811333b11a173fbfe2f4a6f552
sungwoo <54757435+sung-woo-jang@users.noreply.github.com>
2024-08-26 15:53:41 +0900

    세팅 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+    const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    reactStrictMode: true,
+    basePath: isProduction ? '/ghp' : '',
+    // next 이미지 최적화 옵션 off
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -5,28 +5,29 @@ import redRacket from "@/../public/img/redRacket.png";
 import blackRacket from "@/../public/img/blackRacket.png";
 import Image from "next/image";
 interface ProjectPageProps {
-  params: { id: 1 | 2 | 3 };
+  params: { id: "1" | "2" | "3" };
 }
+
+const projectData = {
+  1: {
+    title: "프로젝트 1",
+    description: "프로젝트 1에 대한 상세 설명",
+    image: redRacket,
+  },
+  2: {
+    title: "프로젝트 2",
+    description: "프로젝트 2에 대한 상세 설명",
+    image: blueRacket,
+  },
+  3: {
+    title: "프로젝트 3",
+    description: "프로젝트 3에 대한 상세 설명",
+    image: blackRacket,
+  },
+};
 
 export default function Project({ params }: ProjectPageProps) {
   // 이 부분에서 프로젝트 ID에 따라 실제 프로젝트 데이터를 불러오는 로직을 구현할 수 있습니다.
-  const projectData = {
-    1: {
-      title: "프로젝트 1",
-      description: "프로젝트 1에 대한 상세 설명",
-      image: redRacket,
-    },
-    2: {
-      title: "프로젝트 2",
-      description: "프로젝트 2에 대한 상세 설명",
-      image: blueRacket,
-    },
-    3: {
-      title: "프로젝트 3",
-      description: "프로젝트 3에 대한 상세 설명",
-      image: blackRacket,
-    },
-  };
 
   const project = projectData[params.id];
 
@@ -50,4 +51,11 @@ export default function Project({ params }: ProjectPageProps) {
       </Link>
     </div>
   );
+}
+
+// 정적 경로 생성을 위한 함수
+export function generateStaticParams() {
+  return Object.keys(projectData).map((id) => ({
+    id: id.toString(),
+  }));
 }
